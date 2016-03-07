764b4a1fc9aa8a7d4706ced0ae1c056798795642
Michal Wiadoomski <wiadzix11@gmail.com>
2016-03-07 06:55:35 +0100

    Sprawozdanie i algrytm nx2

--- Lab1/src/Lab1.cpp
+++ Lab1/src/Lab1.cpp
@@ -41,7 +41,7 @@ public:
     if(in>=n)
       {
 	int* Tab1=new int [n]; 
-	for(int i=0; i<n;i++) //przepisuje tablice
+	for(int i=0; i<n;i++) //przepisuje
 	  {
 	    Tab1[i]=Tab[i];
 	  }
@@ -64,6 +64,50 @@ public:
       }
   }
 
+void dopiszx2(int in, int x)
+  {
+    if(in>=n)
+      {
+	int* Tab1=new int [n]; 
+	for(int i=0; i<n;i++) //przepisuje tablice
+	  {
+	    Tab1[i]=Tab[i];
+	  }
+	
+	Tab= new int[n*2];  //tworze nowa powiekszona o 1
+	
+	for(int i=0; i<n;i++)
+	  {
+	    Tab[i]=Tab1[i];
+	  }
+	
+	Tab[n]=x;
+        delete[] Tab1;
+	Tab1=NULL;
+	n=n*2;
+      }
+    else
+      {
+	Tab[in]=x;
+      }
+  }
+
+  void dotnij(int in) //metoda docina rozmiar tablicy do n
+  {
+    int* Tab1=new int [in]; 
+    for(int i=0; i<in;i++) //przepisuje tablice
+      {
+	Tab1[i]=Tab[i];
+      }
+	
+    Tab= new int[in];  //tworze nowa powiekszona o 1
+    
+    for(int i=0; i<in;i++)
+      {
+	Tab[i]=Tab1[i];
+      }
+    n=in;
+  }
 
     void wyswietl()
   {
@@ -73,7 +117,8 @@ public:
       }
     cout << endl;
   }
-  
+
+
 };
 
 
@@ -89,13 +134,19 @@ int main()
   
   cout << "Do jakiego rozmiaru powiekszyc?" << endl;
   cin >> b;
-  
+
+  int i;  
+
   start=clock();
-  for (int i=0;i<b;i++)
+  for (i=0;i<b;i++)
     {
-      dyn.dopisz(i,i+1);
+      dyn.dopiszx2(i,i+1);
     }
+  dyn.dotnij(i);
   stop=clock();
+
+  // dyn.wyswietl();
+  
 
   cout <<"Czas wykonania algorytmu, to: ";
   cout << 1000./CLOCKS_PER_SEC*(stop-start) << "ms" << endl; 
